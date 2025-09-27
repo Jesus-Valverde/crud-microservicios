940558aff000838410ab9808f72cb28eb7f8250f
Jesus Valverde <jesusvalverde.dev@outlook.com>
2025-09-27 02:16:08 -0700

    Agregada la clase usuariosApiService con conexion y primera funcion con prueba de funcionamiento en app.py

--- server/app.py
+++ server/app.py
@@ -3,24 +3,20 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import mysql.connector
 from src.constants.config import db_config
+from src.services.apiServices import usuariosApiService
 
 app = Flask(__name__)
 CORS(app) #Habilitar CORS en todas las rutas
+usuarioAPI = usuariosApiService()
 
 @app.route("/")
 def hello_world():
     return jsonify({"hola mundo": "Archivo inicial - API funcionando con Flask"})
 
-# Prueba para verificar conexion en el servidor
-@app.route("/api/usuarios")
+# Prueba para recibir los usuarios del servidor
+@app.route("/api/getUsuarios")
 def get_users():
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM usuarios")
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return {"usuarios": rows}
+    return usuarioAPI.getUsuarios()
 
 if __name__ == '__main__':
     app.run(debug=True) # Flask corre en el puerto 5000 por defecto
--- server/src/services/apiServices.py
+++ server/src/services/apiServices.py
@@ -1,1 +1,21 @@
-#Aquí estará todo el código para el funcionamiento intermediario entre el servidor y el backend
+import mysql.connector
+from src.constants.config import db_config
+
+class usuariosApiService() :
+
+    def __init__(self):
+        """Constructor de la clase: Datos de conexion"""
+        self.db_config = db_config
+
+    def crearConexion(self):
+        """Crea y devuelve la conexion de la DB"""
+        return  mysql.connector.connect(**self.db_config)
+
+    def getUsuarios(self):
+        conx = self.crearConexion()
+        cursor = conx.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM usuarios")
+        rows = cursor.fetchall()
+        cursor.close()
+        conx.close()
+        return {"usuarios": rows}
