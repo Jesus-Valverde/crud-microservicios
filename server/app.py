e7e00837819b5699f5affae0c753464dfb2b3cab
Jesus Valverde <jesusvalverde.dev@outlook.com>
2025-09-27 03:43:43 -0700

    Agregado las funciones y endpoints de los usuarios: autenticarUsuario, registrarUsuario, getUsuarioById, updateUsuario, cambiarEstadoUsuario

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #Este es el código intermediario entre en servidor y el frontend
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import mysql.connector
 from src.constants.config import db_config
@@ -13,10 +13,48 @@ usuarioAPI = usuariosApiService()
 def hello_world():
     return jsonify({"hola mundo": "Archivo inicial - API funcionando con Flask"})
 
-# Prueba para recibir los usuarios del servidor
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.json
+    correo = data.get("correo")
+    contrasena = data.get("contrasena")
+    
+    return jsonify(usuarioAPI.autenticarUsuario(correo, contrasena))
+
+@app.route("/api/registrarUsuario", methods=["POST"])
+def let_user():
+    data = request.json
+    nombre = data.get("nombre")
+    correo = data.get("correo")
+    contrasena = data.get("contrasena")
+    return jsonify(usuarioAPI.registrarUsuario(nombre, correo, contrasena))
+
 @app.route("/api/getUsuarios")
 def get_users():
     return usuarioAPI.getUsuarios()
+
+@app.route("/api/getUsuarioById")
+def get_userById():
+    userId = int(request.args.get('id'))
+    return usuarioAPI.getUsuarioById(userId)
+
+@app.route("/api/updateUsuario", methods=["PUT"])
+def actualizar_usuario():
+    data = request.json
+    id_usuario = data.get("id")
+    nombre = data.get("nombre")
+    correo = data.get("correo")
+    contrasena = data.get("contrasena")
+
+    return jsonify(usuarioAPI.updateUsuario(id_usuario, nombre, correo, contrasena))
+
+@app.route("/api/cambiarEstadoUsuario", methods=["PUT"])
+def cambiar_estado():
+    data = request.json
+    id_usuario = data.get("id")
+    nuevo_estado = data.get("estado")  # 1 o 0
+
+    return jsonify(usuarioAPI.cambiarEstadoUsuario(id_usuario, nuevo_estado))
 
 if __name__ == '__main__':
     app.run(debug=True) # Flask corre en el puerto 5000 por defecto
